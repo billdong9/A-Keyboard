012dbcad4c77b2e4217ed292050fcbb168033771
18510047382 <bill-dong@hotmail.com>
2019-10-17 11:24:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# A-Keyboard
+A-Keyboard是一个小型的 JavaScript 虚拟键盘。
+
+## 安装
+### NPM
+```shell
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'index.js'
+    }
+}
